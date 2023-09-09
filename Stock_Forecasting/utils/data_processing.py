667c5dd641bc8b3b4a8ea9ccb2532e90b1d412e7
russelrk <49132622+russelrk@users.noreply.github.com>
2023-09-08 20:23:42 -0400

    Update data_processing.py

--- Stock_Forecasting/utils/data_processing.py
+++ Stock_Forecasting/utils/data_processing.py
@@ -1,6 +1,17 @@
 import yfinance as yf
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+import logging
+
+# Create a logger with a specific name
+logger = logging.getLogger(__name__)
+
+# Configure logger settings (you can customize these settings)
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level to INFO
+    format='%(asctime)s [%(levelname)s] %(module)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 def download_stock_data(symbol, start_date, end_date):
     try:
@@ -8,7 +19,8 @@ def download_stock_data(symbol, start_date, end_date):
         stock_data = yf.download(symbol, start=start_date, end=end_date)
         return stock_data
     except Exception as e:
-        print(f"Error downloading stock data: {e}")
+        # Log the exception with a stack trace
+        logger.error(f"Error downloading stock data: {e}", exc_info=True)
         return None
 
 def preprocess_data(data):
@@ -19,7 +31,8 @@ def preprocess_data(data):
         features_scaled = scaler.fit_transform(features)
         return features_scaled
     except Exception as e:
-        print(f"Error preprocessing data: {e}")
+        # Log the exception with a stack trace
+        logger.error(f"Error preprocessing data: {e}", exc_info=True)
         return None
 
 def prepare_sequences(features_scaled, n_steps):
@@ -32,7 +45,8 @@ def prepare_sequences(features_scaled, n_steps):
             y_close.append([features_scaled[i, 1]])  # Closing price
         return np.array(X), np.array(y_open), np.array(y_close)
     except Exception as e:
-        print(f"Error preparing sequences: {e}")
+        # Log the exception with a stack trace
+        logger.error(f"Error preparing sequences: {e}", exc_info=True)
         return None, None, None
 
 def split_data(X, y_open, y_close, split_ratio):
@@ -44,6 +58,6 @@ def split_data(X, y_open, y_close, split_ratio):
         y_close_train, y_close_test = y_close[:split_index], y_close[split_index:]
         return X_train, X_test, y_open_train, y_open_test, y_close_train, y_close_test
     except Exception as e:
-        print(f"Error splitting data: {e}")
+        # Log the exception with a stack trace
+        logger.error(f"Error splitting data: {e}", exc_info=True)
         return None, None, None, None, None, None
-
